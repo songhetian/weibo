78f33e9999f7bc69b65acb1f072f2c71a9d3f0cb
tianhesong <419097924@qq.com>
2023-02-20 06:04:48 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -37,4 +37,32 @@ class UsersController extends Controller
 
         return redirect()->route('users.show', [$user]);
     }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        //验证
+        $this->validate($request, [
+            'name' => 'required|unique:users|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+
+        $data['name'] = $request->name;
+
+        if ($request->has('password')) {
+            $data['password'] = bcrypt($request->password);
+        }
+
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $user);
+    }
 }
